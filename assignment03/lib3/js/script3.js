98b492fcb86ed67fc3bced5a93bcc4f98046c772
georgialeetee <georgiafishy@aol.com>
2014-06-05 02:24:58 +0200

    Assignments

---
+++ assignment03/lib3/js/script3.js
@@ -0,0 +1,9 @@
+
+function displayMessage(firstName) {
+    alert("Hi " + firstName + ", hope you enjoy math riddles!")
+}
+
+function myFunction() {
+    var y = 2987;
+    document.getElementById("demo").innerHTML = y * 2987;
+}
